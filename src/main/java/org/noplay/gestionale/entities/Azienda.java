7ed033e76b26ed278a7636cf0f0c8cf9732ef292
Fabrizio Gullotta <gullottafabrizio@gmail.com>
2021-01-18 17:55:59 +0100

    Fix caricamento classe

---
+++ src/main/java/org/noplay/gestionale/entities/Azienda.java
@@ -0,0 +1,103 @@
+package org.noplay.gestionale.entities;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Azienda {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String ragionesociale;
+	private String partitaiva;
+	private String indirizzo;
+	private String email;
+	private int ntelefono;
+	private List<Dipendente> dipendenti;
+	
+	
+	public Azienda(String ragionesociale, String partitaiva, String indirizzo, String email, int ntelefono,
+			List<Dipendente> dipendenti) {
+		super();
+		this.ragionesociale = ragionesociale;
+		this.partitaiva = partitaiva;
+		this.indirizzo = indirizzo;
+		this.email = email;
+		this.ntelefono = ntelefono;
+		this.dipendenti = dipendenti;
+	}
+
+
+	public Azienda() {
+		super();
+	}
+
+
+	public String getRagionesociale() {
+		return ragionesociale;
+	}
+
+
+	public void setRagionesociale(String ragionesociale) {
+		this.ragionesociale = ragionesociale;
+	}
+
+
+	public String getPartitaiva() {
+		return partitaiva;
+	}
+
+
+	public void setPartitaiva(String partitaiva) {
+		this.partitaiva = partitaiva;
+	}
+
+
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+
+
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public int getNtelefono() {
+		return ntelefono;
+	}
+
+
+	public void setNtelefono(int ntelefono) {
+		this.ntelefono = ntelefono;
+	}
+
+
+	public List<Dipendente> getDipendenti() {
+		return dipendenti;
+	}
+
+
+	public void setDipendenti(List<Dipendente> dipendenti) {
+		this.dipendenti = dipendenti;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/java/org/noplay/gestionale/entities/Dipendente.java
@@ -0,0 +1,131 @@
+package org.noplay.gestionale.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Dipendente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String nome;
+	private String  cognome;
+	private String ddn;
+	private double stipendio;
+	private String dataassunzione;
+	private String ruolo;
+	@ManyToOne
+	@JoinColumn(name = "azienda_id", nullable = false)
+	private Azienda azienda;
+	
+	
+	public Dipendente(long id, String nome, String cognome, String ddn, double stipendio, String dataassunzione,
+			String ruolo, Azienda azienda) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.ddn = ddn;
+		this.stipendio = stipendio;
+		this.dataassunzione = dataassunzione;
+		this.ruolo = ruolo;
+		this.azienda = azienda;
+	}
+
+
+	public Dipendente() {
+		super();
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getCognome() {
+		return cognome;
+	}
+
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+
+	public String getDdn() {
+		return ddn;
+	}
+
+
+	public void setDdn(String ddn) {
+		this.ddn = ddn;
+	}
+
+
+	public double getStipendio() {
+		return stipendio;
+	}
+
+
+	public void setStipendio(double stipendio) {
+		this.stipendio = stipendio;
+	}
+
+
+	public String getDataassunzione() {
+		return dataassunzione;
+	}
+
+
+	public void setDataassunzione(String dataassunzione) {
+		this.dataassunzione = dataassunzione;
+	}
+
+
+	public String getRuolo() {
+		return ruolo;
+	}
+
+
+	public void setRuolo(String ruolo) {
+		this.ruolo = ruolo;
+	}
+
+
+	public Azienda getAzienda() {
+		return azienda;
+	}
+
+
+	public void setAzienda(Azienda azienda) {
+		this.azienda = azienda;
+	}
+	
+	
+	
+	
+	
+	
+
+}
