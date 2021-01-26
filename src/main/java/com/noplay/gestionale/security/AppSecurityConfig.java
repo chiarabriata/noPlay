9578dd79178feaad3ab1d4794174484fcebf543a
Fabrizio Gullotta <gullottafabrizio@gmail.com>
2021-01-26 17:28:40 +0100

    Update AppSecurityConfig.java

--- src/main/java/com/noplay/gestionale/security/AppSecurityConfig.java
+++ src/main/java/com/noplay/gestionale/security/AppSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -31,7 +32,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter
     {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
         return provider;
     }
 
